800debefb349142ae366019d558b7f066f2d6911
Reed Rosenberg <spacecoffin@users.noreply.github.com>
2018-11-09 20:29:17 -0500

    First draft of trunc

--- 1-Getting-Started-with-Go/module-2/trunc/trunc.go
+++ 1-Getting-Started-with-Go/module-2/trunc/trunc.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+/*
+Write a program which prompts the user to enter a floating point
+number and prints the integer which is a truncated version of the
+floating point number that was entered. Truncation is the process of
+removing the digits to the right of the decimal place.
+*/
 func main() {
-	fmt.Println("vim-go")
+	var userInput float64
+	fmt.Println("Please enter a floating point number")
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		fmt.Println("Invalid input")
+		fmt.Println(err)
+		return
+	}
+	castAsInt := int(userInput)
+	fmt.Printf("%d", castAsInt)
 }
