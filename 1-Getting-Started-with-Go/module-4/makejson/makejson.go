b489f24de7c585e09655489e233768449cb6930a
Reed Rosenberg <spacecoffin@users.noreply.github.com>
2018-11-19 21:48:53 -0500

    Goes back to map method.

--- 1-Getting-Started-with-Go/module-4/makejson/makejson.go
+++ 1-Getting-Started-with-Go/module-4/makejson/makejson.go
@@ -7,17 +7,11 @@ import (
 	"os"
 )
 
-// Person struct to unmarshall JSON into
-type Person struct {
-	Name    string
-	Address string
-}
-
 /*
 Write a program which prompts the user to first enter a name, and then enter an address. Your program should create a map and add the name and address to the map using the keys "name" and "address", respectively. Your program should use Marshal() to create a JSON object from the map, and then your program should print the JSON object.
 */
 func main() {
-	var person Person
+	var unmarshalledMap map[string]string
 	personMap := make(map[string]string)
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Please enter a name: ")
@@ -39,9 +33,9 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	if err := json.Unmarshal(barr, &person); err != nil {
+	if err := json.Unmarshal(barr, &unmarshalledMap); err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("\nUnmarshalled JSON as Go object:\n%+v\n", person)
+	fmt.Printf("\nUnmarshalled JSON as Go object:\n%v\n", unmarshalledMap)
 }
